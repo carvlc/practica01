56afda99b88f540988899675f941f7c97a0b722d
carvlc <cnsvilca@gmail.com>
2023-11-09 22:52:15 -0300

    Update: se agrego Esfera BalaPlayer y su material, se creo el script BalaBehaviour para el movimiento de la bala

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    private float speedZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedZ = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,speedZ * Time.deltaTime);
+    }
+}
